45cfddf28a04777bd7d9d400fd4fc510df4cafea
Kid Vader <nickjamil@gmail.com>
2025-07-05 17:53:48 -0400

    Removed init requirement from RotationMechanics protocol as it's not needed.

--- Sources/Tetris/RotationMechanics.swift
+++ Sources/Tetris/RotationMechanics.swift
@@ -1,6 +1,5 @@
 public protocol RotationMechanics {
 
-    init(playfieldWidth: Int, playfieldHeight: Int)
     func getSpawnCoordinates(_ tetromino: Tetromino) -> Coordinates
     func rotate(
         _ tetromino: Tetromino, 
